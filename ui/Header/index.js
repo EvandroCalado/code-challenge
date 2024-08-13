ccbc0a9453b0af6f3e84d374e44b28bfd410138d
Evandro Calado <evandrocalado07@gmail.com>
2024-08-12 22:44:40 -0300

    🐳 chore: add link

--- ui/Header/index.js
+++ ui/Header/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 export const Header = ({ onSelectCommunity, communities }) => (
   <header className="w-full bg-woodsmoke-950 p-4">
     <div className="mx-auto flex max-w-7xl items-center justify-between">
-      <h2 className="text-xl font-semibold text-woodsmoke-50 md:text-2xl">
+      <a
+        href="/"
+        className="text-xl font-semibold text-woodsmoke-50 md:text-2xl"
+      >
         Events Challenge
-      </h2>
+      </a>
       <select
         onChange={onSelectCommunity}
         className="rounded border-[1px] border-woodsmoke-50 bg-woodsmoke-950 p-2 text-woodsmoke-50"
