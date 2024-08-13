22f82410909656ba46234ad54415d239afd96eb1
Evandro Calado <evandrocalado07@gmail.com>
2024-08-13 08:23:28 -0300

    🦄 refactor: move commom styles

--- ui/Button/index.js
+++ ui/Button/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
-export const Button = ({ children, type = 'button', onClick, variant }) => {
+export const Button = ({
+  children,
+  type = 'button',
+  onClick,
+  variant,
+  ...props
+}) => {
   const buttonVariants = {
-    primary:
-      'flex items-center gap-2 rounded-md border-[1px] bg-woodsmoke-950 px-4 py-2 text-sm font-semibold text-white duration-150 hover:bg-woodsmoke-950/90',
+    primary: 'bg-woodsmoke-950 text-white hover:bg-woodsmoke-950/90',
 
-    secondary:
-      'flex items-center gap-2 rounded-md border-[1px] border-woodsmoke-300 px-4 py-2 text-sm font-semibold duration-150 hover:bg-woodsmoke-100/30',
+    secondary: 'border-woodsmoke-300 hover:bg-woodsmoke-100/30',
   };
 
   return (
-    <button type={type} onClick={onClick} className={buttonVariants[variant]}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${buttonVariants[variant]} flex items-center gap-2 rounded-md border-[1px] px-4 py-2 text-sm font-semibold duration-150 disabled:pointer-events-none disabled:bg-woodsmoke-200 disabled:hover:bg-woodsmoke-200`}
+      {...props}
+    >
       {children}
     </button>
   );
